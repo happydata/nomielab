e29715b395c20c8b28efd91a1c2049054bcdb655
Brandon Corbin <brandon@icorbin.com>
2016-01-09 11:43:41 -0500

    Removing JSDoc requirements

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
-var jsdoc       = require('gulp-jsdoc');
 var electron    = require('gulp-electron');
 var packageJson = require('./package.json');
 
@@ -26,38 +25,38 @@ gulp.task('sass', function() {
         .pipe(browserSync.stream());
 });
 
-gulp.task('electron', function() {
-    gulp.src("")
-    .pipe(electron({
-        src: './src',
-        packageJson: packageJson,
-        release: './electron/release',
-        cache: './electron/cache',
-        version: 'v0.27.0',
-        packaging: true,
-        platforms: ['win32-ia32', 'darwin-x64'],
-        platformResources: {
-            darwin: {
-                CFBundleDisplayName: packageJson.name,
-                CFBundleIdentifier: packageJson.name,
-                CFBundleName: packageJson.name,
-                CFBundleVersion: packageJson.version,
-                iconx: 'gulp-electron.icns'
-            },
-            win: {
-                "version-string": packageJson.version,
-                "file-version": packageJson.version,
-                "product-version": packageJson.version,
-                "iconx": 'gulp-electron.ico'
-            }
-        }
-    }))
-    .pipe(gulp.dest(""));
-});
+// gulp.task('electron', function() {
+//     gulp.src("")
+//     .pipe(electron({
+//         src: './src',
+//         packageJson: packageJson,
+//         release: './electron/release',
+//         cache: './electron/cache',
+//         version: 'v0.27.0',
+//         packaging: true,
+//         platforms: ['win32-ia32', 'darwin-x64'],
+//         platformResources: {
+//             darwin: {
+//                 CFBundleDisplayName: packageJson.name,
+//                 CFBundleIdentifier: packageJson.name,
+//                 CFBundleName: packageJson.name,
+//                 CFBundleVersion: packageJson.version,
+//                 iconx: 'gulp-electron.icns'
+//             },
+//             win: {
+//                 "version-string": packageJson.version,
+//                 "file-version": packageJson.version,
+//                 "product-version": packageJson.version,
+//                 "iconx": 'gulp-electron.ico'
+//             }
+//         }
+//     }))
+//     .pipe(gulp.dest(""));
+// });
 
-gulp.task('docs', function() {
-  return gulp.src("./app/**/**.js")
-  .pipe(jsdoc('./docs/js'));
-});
+// gulp.task('docs', function() {
+//   return gulp.src("./app/**/**.js")
+//   .pipe(jsdoc('./docs/js'));
+// });
 
 gulp.task('default', ['serve']);
